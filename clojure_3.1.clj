8aae7262c5407e8b826b3bbdac1e7e65083b555a
unknown <boris.burtseffyandex.ru>
2021-11-22 00:29:25 +0700

    almost work 3

--- clojure_3.1.clj
+++ clojure_3.1.clj
@@ -1,40 +1,46 @@
-(defn cancatOnParts [coll count-parts]
-  (let [count-in-part (/ (count coll) count-parts)]
-  (loop [count-parts count-parts coll coll result '()]
-    (if (or (empty? coll) (< count-parts 0))
-      result
-      (recur (dec count-parts)
-             (drop count-in-part coll)
-             (cons (take count-in-part coll) result))))))
+(defn partition-lazy [chunk-size coll]
+  (lazy-seq
+   (if (empty? coll)
+     nil
+     (cons
+      (take chunk-size coll)
+      (partition-lazy
+       chunk-size
+       (drop chunk-size coll))))))
 
-(def coll '("a" "aa" "b" "n" "f" "lisp" "clojure" "q" ""))
+(defn parallel-filter [pred treads chunk-size coll]
+  (mapcat deref
+          (doall (map
+                  #(future (doall (filter pred %)))
+                   (partition-lazy 
+                    chunk-size 
+                    coll)))))
 
-(cancatOnParts '("a" "aa" "b" "n" "f" "lisp" "clojure" "q" "" "d" "e" "s" "q") 4)
+(defn parallel-filter [pred treads chunk-size coll]
+  (mapcat deref
+  ;;(mapcat (fn [x] x)
+          (mapcat identity
+                  (doall (map (fn [colls]
+                                (map (fn [pred-colls]
+                                       ;;(println pred-colls)
+                                       (future (doall (filter pred pred-colls))))
+                                     (partition-lazy chunk-size colls)))
+                              (partition-lazy
+                               (* chunk-size treads)
+                               coll))))))
 
-(defn parallel-filter [pred coll]
-  (map #(future (map pred %)) (cancatOnParts coll 4)))
+(take 10 (partition-lazy 10 (take 2 (range))))
 
-(defn parallel-filter [pred function limit]
-  (map #(future (map pred %)) (cancatOnParts (take limit function) 4)))
+(parallel-filter (fn [x]
+                   (= (count x) 1))
+                 4
+                 (lazy-seq coll-1))
 
-(cancatOnParts (take 10000 (range)) 4)
-
-(parallel-filter (fn [x] (= (count x) 1)) (range) 10000)
-
-(take 3 (range))
-
-(take 4 coll)
-
-(take 4 (drop 4 coll))
-
-(/ (count coll) 4)
-
-(let [coll ["a" "aa" "b" "n" "f" "lisp" "clojure" "q" ""]])
-
-(filter
- #(= (count %) 1)
- ["a" "aa" "b" "n" "f" "lisp" "clojure" "q" ""])
-
-(parallel-filter
- #(= (count %) 1)
- ["a" "aa" "b" "n" "f" "lisp" "clojure" "q" ""])
+(time (->> (iterate inc 0)
+           (take 20)
+           (parallel-filter (fn [x]
+                              (Thread/sleep 100)
+                              (odd? x))
+                            2
+                            5)
+           (doall)))
